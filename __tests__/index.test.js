367a7e1558c9b7df9e70ebafed4e377b5b3e20d0
Oleg Afanasev <iamafanasev@gmail.com>
2019-11-22 09:02:56 +0300

    add new test with empty files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,6 +7,9 @@ const result = fs.readFileSync('./__fixtures__/diffFlatJson', 'utf-8');
 const filePath1 = './__fixtures__/before.json';
 const filePath2 = './__fixtures__/after.json';
 
+const filePath3 = './__fixtures__/beforeEmpty.json';
+const filePath4 = './__fixtures__/afterEmpty.json';
+
 const absolutePath1 = resolve(filePath1);
 const absolutePath2 = resolve(filePath2);
 
@@ -16,4 +19,5 @@ test('absolutePath', () => {
 
 test('JSON flat compare', () => {
   expect(genDiff(filePath1, filePath2)).toEqual(result);
+  expect(genDiff(filePath3, filePath4)).toEqual('{\n  \n}');
 });
