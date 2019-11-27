ef3284c81187f44b818f96d704b1a79f24d72941
Oleg Afanasev <iamafanasev@gmail.com>
2019-11-27 15:57:28 +0300

    add feature Compare Flat ini

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -13,18 +13,27 @@ const flatJson2 = './__fixtures__/after.json';
 const flatYml1 = './__fixtures__/beforeFlat.yml';
 const flatYml2 = './__fixtures__/afterFlat.yml';
 
+const flatIni1 = './__fixtures__/beforeFlat.ini';
+const flatIni2 = './__fixtures__/afterFlat.ini';
+
 const absolutePath1 = resolve(flatJson1);
 const absolutePath2 = resolve(flatJson2);
-console.log();
+
 test('absolutePath', () => {
   expect(genDiff(absolutePath1, absolutePath2)).toEqual(result);
 });
 
-test('JSON flat compare', () => {
-  expect(genDiff(flatJson1, flatJson2)).toEqual(result);
-  expect(genDiff(flatJsonEmpty1, flatJsonEmpty2)).toEqual('{\n  \n}');
-});
+describe('flat file compar', () => {
+  test('JSON compare', () => {
+    expect(genDiff(flatJson1, flatJson2)).toEqual(result);
+    expect(genDiff(flatJsonEmpty1, flatJsonEmpty2)).toEqual('{\n  \n}');
+  });
 
-test('YML flat compare', () => {
-  expect(genDiff(flatYml1, flatYml2)).toEqual(result);
+  test('YML compare', () => {
+    expect(genDiff(flatYml1, flatYml2)).toEqual(result);
+  });
+
+  test('ini compare', () => {
+    expect(genDiff(flatIni1, flatIni2)).toEqual(result);
+  });
 });
--- src/parsers.js
+++ src/parsers.js
@@ -1,7 +1,9 @@
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 const mapping = {
   yml: yaml.safeLoad,
+  ini: ini.parse,
   json: JSON.parse,
 };
 
