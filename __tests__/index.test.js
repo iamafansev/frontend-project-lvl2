f7b0d8983d5f2692609fdfda297ca5f7d38615ff
Oleg Afanasev <iamafanasev@gmail.com>
2019-11-29 12:39:38 +0300

    change test and fixtures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,46 +2,41 @@ import fs from 'fs';
 import { resolve } from 'path';
 import genDiff from '../src';
 
-const result = fs.readFileSync('./__fixtures__/diffFlatJson', 'utf-8');
-const result2 = fs.readFileSync('./__fixtures__/diff', 'utf-8');
+const result = fs.readFileSync('./__fixtures__/diff', 'utf-8');
 
-const flatJsonEmpty1 = './__fixtures__/beforeEmpty.json';
-const flatJsonEmpty2 = './__fixtures__/afterEmpty.json';
+const beforeEmpty = './__fixtures__/beforeEmpty.json';
+const afterEmpty = './__fixtures__/afterEmpty.json';
 
-const flatJson1 = './__fixtures__/beforeFlat.json';
-const flatJson2 = './__fixtures__/afterFlat.json';
+const beforeJson = './__fixtures__/before.json';
+const afterJson = './__fixtures__/after.json';
 
-const flatYml1 = './__fixtures__/beforeFlat.yml';
-const flatYml2 = './__fixtures__/afterFlat.yml';
+const beforeYml = './__fixtures__/before.yml';
+const afterYml = './__fixtures__/after.yml';
 
-const flatIni1 = './__fixtures__/beforeFlat.ini';
-const flatIni2 = './__fixtures__/afterFlat.ini';
+// const beforeIni = './__fixtures__/before.ini';
+// const afterIni = './__fixtures__/after.ini';
 
-const absolutePath1 = resolve(flatJson1);
-const absolutePath2 = resolve(flatJson2);
+const absolutePath1 = resolve(beforeJson);
+const absolutePath2 = resolve(afterJson);
 
 test('absolutePath', () => {
   expect(genDiff(absolutePath1, absolutePath2)).toEqual(result);
 });
 
-describe('flat file compar', () => {
+test('compare empty files', () => {
+  expect(genDiff(beforeEmpty, afterEmpty)).toEqual('{\n}');
+});
+
+describe('compar files', () => {
   test('JSON compare', () => {
-    expect(genDiff(flatJson1, flatJson2)).toEqual(result);
-    expect(genDiff(flatJsonEmpty1, flatJsonEmpty2)).toEqual('{\n}');
+    expect(genDiff(beforeJson, afterJson)).toEqual(result);
   });
 
   test('YML compare', () => {
-    expect(genDiff(flatYml1, flatYml2)).toEqual(result);
+    expect(genDiff(beforeYml, afterYml)).toEqual(result);
   });
 
-  test('ini compare', () => {
-    expect(genDiff(flatIni1, flatIni2)).toEqual(result);
-  });
-});
-
-describe('file compar', () => {
-  test('JSON compare', () => {
-    expect(genDiff('./__fixtures__/before.json', './__fixtures__/after.json')).toEqual(result2);
-    expect(genDiff(flatJsonEmpty1, flatJsonEmpty2)).toEqual('{\n}');
-  });
+  // test('ini compare', () => {
+  //   expect(genDiff(flatIni1, flatIni2)).toEqual(result);
+  // });
 });
