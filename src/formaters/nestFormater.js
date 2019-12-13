04e04e6d220f6c3177a050efb0a911cd4f1ebb38
Oleg Afanasev <iamafanasev@gmail.com>
2019-12-13 13:37:20 +0300

    change implementation nestFormater

--- src/formaters/nestFormater.js
+++ src/formaters/nestFormater.js
@@ -1,4 +1,14 @@
+import _ from 'lodash';
+
 const tab = '  ';
+
+const buildString = (key, value, tabCount, statusChar = '') => {
+  if (value instanceof Object) {
+    return `${tab.repeat(tabCount)}${statusChar} ${key}: ${stringify(value, tabCount + 2)}`; // eslint-disable-line no-use-before-define
+  }
+
+  return `${tab.repeat(tabCount)}${statusChar} ${key}: ${value}`;
+};
 
 const stringify = (ast, tabCount) => {
   const keys = Object.keys(ast);
@@ -6,51 +16,52 @@ const stringify = (ast, tabCount) => {
   const result = keys.reduce((acc, key) => {
     const value = ast[key];
 
-    if (value instanceof Object) {
-      return `${acc}\n${tab.repeat(tabCount + 1)}${key}: ${stringify(value, tabCount + 2)}`;
-    }
+    return [...acc, `\n${buildString(key, value, tabCount, ' ')}`];
+  }, []);
 
-    return `${acc}\n${tab.repeat(tabCount + 1)}${key}: ${value}`;
-  }, '');
-
-  return `{${result}\n${tab.repeat(tabCount - 1)}}`;
+  return `{${result.join('')}\n${tab.repeat(tabCount - 1)}}`;
 };
 
-const buildValueString = (name, values, tabCount) => {
+const typeActions = [
+  {
+    check: (arg) => arg === 'unchanged',
+    processBuildString: (key, value, tabCount) => buildString(key, value, tabCount, ' '),
+  },
+  {
+    check: (arg) => arg === 'added',
+    processBuildString: (key, value, tabCount) => buildString(key, value, tabCount, '+'),
+  },
+  {
+    check: (arg) => arg === 'removed',
+    processBuildString: (key, value, tabCount) => buildString(key, value, tabCount, '-'),
+  },
+];
+
+const getProcessType = (arg) => _.find(typeActions, (({ check }) => check(arg)));
+
+const processValue = (name, values, tabCount) => {
   const keys = Object.keys(values);
 
-  return keys.map((key) => {
-    if (key === 'unchanged') {
-      if (values[key] instanceof Object) {
-        return `\n${tab.repeat(tabCount)}  ${name}: ${stringify(values[key], tabCount + 2)}`;
-      }
-      return `\n${tab.repeat(tabCount)}  ${name}: ${values[key]}`;
-    }
+  const result = keys.map((key) => {
+    const value = values[key];
+    const { processBuildString } = getProcessType(key);
 
-    if (key === 'added') {
-      if (values[key] instanceof Object) {
-        return `\n${tab.repeat(tabCount)}+ ${name}: ${stringify(values[key], tabCount + 2)}`;
-      }
-      return `\n${tab.repeat(tabCount)}+ ${name}: ${values[key]}`;
-    }
+    return processBuildString(name, value, tabCount);
+  });
 
-    if (values[key] instanceof Object) {
-      return `\n${tab.repeat(tabCount)}- ${name}: ${stringify(values[key], tabCount + 2)}`;
-    }
-    return `\n${tab.repeat(tabCount)}- ${name}: ${values[key]}`;
-  }).join('');
+  return result.join('\n');
 };
 
 const renderDiff = (ast, tabCount = 1) => {
   const result = ast.reduce((acc, [key, value]) => {
     if (value instanceof Array) {
-      return `${acc}\n${tab.repeat(tabCount)}  ${key}: ${renderDiff(value, tabCount + 2)}`;
+      return [...acc, `${tab.repeat(tabCount)}  ${key}: ${renderDiff(value, tabCount + 2)}`];
     }
 
-    return `${acc}${buildValueString(key, value, tabCount)}`;
-  }, '');
+    return [...acc, `${processValue(key, value, tabCount)}`];
+  }, []);
 
-  return `{${result}\n${tab.repeat(tabCount - 1)}}`;
+  return `{\n${result.join('\n')}\n${tab.repeat(tabCount - 1)}}`;
 };
 
 export default renderDiff;
